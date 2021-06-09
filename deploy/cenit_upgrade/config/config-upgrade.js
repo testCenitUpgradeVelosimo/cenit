6b4bc73c75e42766518138e93770189f6754a31a
Ramon A. Anglada <anglada.alex@gmail.com>
2021-06-09 11:18:43 -0400

    devops - mongoid-atlas for cenitV2.

--- deploy/cenit_upgrade/config/config-upgrade.js
+++ deploy/cenit_upgrade/config/config-upgrade.js
@@ -1,6 +1,6 @@
 window.appConfig = {
     useEnvironmentConfig: false,
-    localhost: 'http://cenit-ui-upgrade.velosimo.io.s3-website-us-east-1.amazonaws.com',
+    localhost: 'https://cenit-ui-upgrade.velosimo.io',
     cenitHost: 'https://back-cenit-upgrade.velosimo.io',
     timeoutSpan: 300000,
     appIdentifier: 'admin'
